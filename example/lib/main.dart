8bd6a939c1faf4fa6702a8d536612f68857e6c7d
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2022-09-10 02:02:50 +0000

    Import embla session in example to ensure that client build triggers errors

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,24 @@
+/*
+ * This file is part of the Embla Core Flutter package
+ * Copyright (c) 2022 Miðeind ehf. <mideind@mideind.is>
+ * Original author: Sveinbjorn Thordarson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import 'package:flutter/material.dart';
+
+import 'package:embla_core/embla_core.dart' show EmblaSession;
 
 void main() {
   runApp(const MyApp());
